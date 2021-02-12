6a67a9cd324832d2aa1bc8e86cd03f4999a861d7
Fabien_V_LPDIM <fabien.vauclin@outlook.fr>
2021-02-12 22:21:31 +0100

    Replace calls SQL with methods

--- PHP TP1/index.php
+++ PHP TP1/index.php
@@ -1,63 +1,38 @@
 <?php
+
 use App\model\Database;
 use App\model\TaskRepository;
 
 require 'vendor/autoload.php';
 define("DATABASE_FILE", "./data.db");
+
 $table = "tasks";
 $tasks = [];
-if (!file_exists(DATABASE_FILE)) {
-    $database = new SQLite3(DATABASE_FILE);
-    $database->exec(<<<SQL
-        create table $table 
-        (
-            id INTEGER
-                constraint tasks_pk
-                    primary key autoincrement,
-            name TEXT,
-            checked INTEGER default 0
-        );
-        INSERT INTO $table (id, name, checked) VALUES (1, 'Task to be done', 0);
-        INSERT INTO $table (id, name, checked) VALUES (2, 'Task done', 1);
-        
-SQL
-    );
-    header("Location: /");
-}
-$database = new SQLite3(DATABASE_FILE);
-?>
+Database::initialize(__DIR__ . DIRECTORY_SEPARATOR. DATABASE_FILE);
 
-<?php
+$taskRepository = new TaskRepository();
+$taskRepository->initialize();
 
 if (isset($_GET["action"])) {
     switch ($_GET["action"]) {
         case "uncheck":
             if (isset($_GET["id"])) {
                 $id = $_GET["id"];
-                $database->exec(<<<SQL
-UPDATE $table set checked=0 WHERE id=$id;
-SQL
-                );
+                $taskRepository->update($id);
             }
             header("Location: /");
             break;
         case "check":
             if (isset($_GET["id"])) {
                 $id = $_GET["id"];
-                $database->exec(<<<SQL
-UPDATE $table set checked=1 WHERE id=$id;
-SQL
-                );
+                $taskRepository->update($id, true);
             }
             header("Location: /");
             break;
         case "delete":
             if (isset($_GET["id"])) {
                 $id = $_GET["id"];
-                $database->exec(<<<SQL
-DELETE FROM $table WHERE id=$id;
-SQL
-                );
+                $taskRepository->delete($id);
             }
             header("Location: /");
             break;
@@ -65,10 +40,7 @@ SQL
             if (isset($_GET["name"])) {
                 $name = $_GET["name"];
                 $name = addslashes($name);
-                $database->exec(<<<SQL
-                INSERT INTO $table (name) values('$name')
-SQL
-                );
+                $taskRepository->add($name);
             }
             header("Location: /");
             break;
@@ -78,16 +50,12 @@ SQL
     }
 }
 
-
-$query = $database->query(<<<SQL
-    SELECT * FROM $table ORDER BY checked DESC;
-SQL
-);
+$query = $taskRepository->getAll();
 if (!$query)
     die("Impossible to execute query.");
 
-while ($row = $query->fetchArray(SQLITE3_ASSOC)) {
+while ($row = $query->fetch()) {
     $tasks[] = $row;
 }
-?>
 
+require 'template.php';
